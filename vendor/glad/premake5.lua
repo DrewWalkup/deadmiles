495938354e7ac11437e7976b62233fe3b00dcae9
dnwalkup <drew.walkup@gmail.com>
2020-07-22 05:38:47 -0400

    GLAD lib for OpenGl 4.6

---
+++ vendor/glad/premake5.lua
@@ -0,0 +1,23 @@
+project "GLAD"
+	kind "StaticLib"
+	language "C"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"include/**.h",
+		"include/**.h",
+		"src/**.c"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
